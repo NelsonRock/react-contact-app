2de20d9af0055deb0e45628d07817bcf53ad5f8e
Nelson Rodriguez <nelsao101@gmail.com>
2016-05-20 14:35:58 +0200

    refactoring dossier and added contacts.json for fetching with package whatwg-fetch(just in chrome and firefox) with polyfill it's support for all browser

--- app/ContactApp.js
+++ app/ContactApp.js
@@ -34,13 +34,5 @@ ContactApp.propTypes = {
   contacts: PropTypes.arrayOf(PropTypes.object),
   filterText: PropTypes.string,
 }
-let contacts = [
-{ name: "Cassio Zen", email: "cassiozen@gmail.com" },
-{ name: "Dan Abramov", email: "gaearon@somewhere.com" },
-{ name: "Pete Hunt", email: "floydophone@somewhere.com" },
-{ name: "Paul O’Shannessy", email: "zpao@somewhere.com" },
-{ name: "Ryan Florence", email: "rpflorence@somewhere.com" },
-{ name: "Sebastian Markbage", email: "sebmarkbage@here.com" },
-];
 
 ReactDOM.render(<ContactApp contacts={contacts} />, document.getElementById('root'));
